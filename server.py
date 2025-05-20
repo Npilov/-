683db1f11582e020874c791b5692aefeccd51d8e
egorminin <varyminin@yandex.ru>
2025-05-20 14:57:18 +0300

    создание цикла сервера подключение игроков отправка сообщений клиентам

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import time
+main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_IP, socket.TCP_NODELAY,1)
+main_socket.bind(("localhost",10000))
+main_socket.setblocking(False)
+main_socket.listen(5)
+players = []
+while True:
+    try:
+        new_socket,addr = main_socket.accept()
+        new_socket.setblocking(False)
+        players.append(new_socket)
+        print("Подключился",addr)
+    except BlockingIOError:
+        pass
+    for sock in players:
+        try:
+            data = sock.recv(1024).decode()
+            print("Получил",data)
+        except BlockingIOError:
+            pass
+    for sock in players:
+        try:
+            sock.send("LOL".encode())
+        except:
+            players.remove(sock)
+            sock.close()
+    time.sleep(1)
